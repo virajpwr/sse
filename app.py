77655608889065527e1da34e01fa0cf65c698896
Viraj Pawar <viraj.telematicus@gmail.com>
2022-11-07 11:52:35 +0000

    new changes

--- app.py
+++ app.py
@@ -23,15 +23,14 @@ if genre == 'Statistics':
     st.header('Statistics')
     st.subheader('The dataset contains four columns: **Year**, **Month** and **Var A** and **Var B** and 120 rows. The **Year** column ranges from 2010 to 2019 and the **Month** column ranges from Jan to Dec.')
     st.markdown('Dataset')
-    image_data = Image.open(
-        r'.\plots\descriptive\dataset.jpg')
+    image_data = Image.open('./plots/descriptive/dataset.jpg')
     st.image(image_data, width=250, caption='Dataset')
 
     st.text('----------------------- Descriptive statistics of the dataset------------------------**')
     st.markdown('The image below shows the descriptive statistics of the Var A and Var B. The **mean** and **Std deviation** of Var B is higher than Var A. The **range** of Var A is 791 is higher than and Var B range which 786')
     # insert image
     image_stats = Image.open(
-        r'.\plots\descriptive\stats.jpg')
+        './plots/descriptive/stats.jpg')
     st.image(image_stats, width=250,
              caption='Descriptive statistics of Var A and Var B')
 
@@ -43,32 +42,32 @@ if genre == 'Statistics':
     - Kurtosis of Var B suggest negative kurtosis.
     """)
     image_stats = Image.open(
-        r'.\plots\descriptive\skewness.jpg')
+        './plots/descriptive/skewness.jpg')
     st.image(image_stats, width=250, caption='distribution of Var A and Var B')
     st.text('----------------------- statistical tests------------------------**')
     st.markdown('Independent two sample t-test')
     st.write("Performed independent two sample t-test to check if the means of Var A and Var B are significantly different with alpa = 0.05. The p-value is 0.000 which is less than 0.05. Hence, we reject the null hypothesis and conclude that the means of Var A and Var B are significantly different.")
     image_stats = Image.open(
-        r'.\plots\descriptive\ttest.jpg')
+        './plots/descriptive/ttest.jpg')
     st.image(image_stats, width=600, caption='t-test')
     st.markdown('ANOVA')
     st.write("Performed one way ANOVA to check difference in year and month for variable Var A and Var B with alpha=0.05.")
     st.write('There is no significant difference in the means of Var A for the years 2010 to 2019. However, there is a significant difference in the means of Var A for the months Jan to Dec.')
     image_stats = Image.open(
-        r'.\plots\descriptive\anova.jpg')
+        './plots/descriptive/anova.jpg')
     st.image(image_stats, width=600, caption='ANOVA')
     st.write('There is significant difference in the means of Var B for the years 2010 to 2019 and for the months Jan to Dec.')
     image_stats = Image.open(
-        r'.\plots\descriptive\anova2.jpg')
+        './plots/descriptive/anova2.jpg')
     st.image(image_stats, width=600, caption='ANOVA')
 
 # if genre == 'Trend':
 #     st.header('Box plots')
 #     st.markdown('The box plots below show the distribution of Var A and Var B for the years 2010 to 2019 and for the months Jan to Dec.')
 #     image_stats = Image.open(
-#         r'.\plots\boxplots\boxplot.jpg')
+#         r'./plots/boxplots/boxplot.jpg')
 #     st.image(image_stats, width=600, caption='Box plots')
 #     st.markdown('The box plots below show the distribution of Var A and Var B for the years 2010 to 2019 and for the months Jan to Dec.')
 #     image_stats = Image.open(
-#         r'.\plots\boxplots\boxplot2.jpg')
+#         r'./plots/boxplots/boxplot2.jpg')
 #     st.image(image_stats, width=600, caption='Box plots')
